d49a51696128b6cc5f34d53bb3a0096a82ad8ace
kuaidy <kdy1115@163.com>
2021-07-03 16:35:23 +0800

    添加自定义导航

--- js/function.js
+++ js/function.js
@@ -1,6 +1,15 @@
+var DBNAME="NAVDATABASE";
+var VERSION=1;
+var TBTYPE="SITESTYPESDEFAULT";
+var TBSITES="SITES"
+var db;
+var IndexDefaultType="defaulttype";
+var IndexType="type";
+
+
 //网站
 function getdata(resourcename,tagname){
-	var innercontent='<div class="tags" onclick="ChangeStatus"><span class="titlesign"></span><h3>'+tagname+'</h3></div><div class="sitediv"><ul id="'+resourcename+'" class="sitesul"></ul></div>';
+	var innercontent='<div class="tags '+resourcename+'" onclick="ChangeStatus"><span class="titlesign"></span><h3>'+tagname+'</h3></div><div class="sitediv"><ul id="'+resourcename+'" class="sitesul"></ul></div>';
 	$("#content").append(innercontent);
 
 	var path="resource/sites/"+resourcename+".json";
@@ -249,4 +258,269 @@ function GetMoreData(url){
 		$("#toolcontentul_flex").append(items1);
 
 	});
+}
+
+
+//判断浏览器是否可用indexeddb数据库
+function IsSupportIndexeddb(){
+	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+	
+	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+	window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+	
+	if (!window.indexedDB) {
+		window.alert("您的浏览器不支持indexeddb数据库，无法使用此功能，请使用最新版谷歌，edge，火狐浏览器！")
+		return false;
+	}else{
+		return true;
+	}
+}
+
+//创建一个数据库，版本号为1，表
+function CreateDatabase(src){
+	var request = indexedDB.open(DBNAME,VERSION);
+	
+	request.onerror = function(event) {
+		console.log("打开失败");
+	};
+		
+	request.onupgradeneeded = function(event) {
+		db = event.target.result;
+		if (!db.objectStoreNames.contains(TBSITES)){
+			var objstore=db.createObjectStore(TBSITES, { keyPath:'id',autoIncrement:true });
+			objstore.createIndex('type', 'type', { unique: false });
+		}
+		if (!db.objectStoreNames.contains(TBTYPE)){
+			var objstore=db.createObjectStore(TBTYPE, {keyPath:'id',autoIncrement:true });
+			objstore.createIndex('defaulttype', 'defaulttype', { unique: false });
+		}
+	}
+
+	request.onsuccess = function(event) {
+		db = event.target.result;
+		if(src=="set"){
+			readAll(TBSITES);
+			readAll(TBTYPE);
+		}else{
+			ReadHomeData(TBSITES);
+			ReadHomeData(TBTYPE);
+		}
+		
+		console.log("打开成功");
+		};
+}
+
+//添加内容
+function AddData(obj,data,index,indexvalue){
+	if(db){
+		var transaction = db.transaction([obj], 'readonly');
+		var store = transaction.objectStore(obj);
+		var index = store.index(index);
+		var request = index.get(indexvalue);
+
+		request.onsuccess = function (e) {
+			var result = e.target.result;
+			if (result) {
+				switch(obj){
+					case TBTYPE:
+						result.canuse=data.canuse;
+						break;
+				}
+				Update(obj,result);
+			} else {
+				Add(obj,data);
+			}
+		}
+	}
+};
+
+//添加数据
+function Add(obj,data){
+	if(db){
+		var res = db.transaction([obj],'readwrite').objectStore(obj).add(data);
+		res.onsuccess = function (event) {
+			console.log('数据写入成功');
+			switch(obj){
+				case TBSITES:
+					readAll(obj);
+					break;
+			}
+			
+		};
+		res.onerror = function (event) {
+			console.log('数据写入失败');
+		}
+	}
+}
+
+//更新数据
+function Update(obj,data){
+	if(db){
+		var res = db.transaction([obj], 'readwrite').objectStore(obj).put(data);
+		res.onsuccess = function (e) {
+			console.log('数据更新成功');
+		}
+
+		res.onerror = function (event) {
+			console.log('数据更新失败');
+		}
+	}
+}
+
+//删除数据
+function Delete(obj,id){
+	if(db){
+		var request = db.transaction([obj], 'readwrite').objectStore(obj).delete(Number(id));
+		request.onsuccess = function (event) {
+			switch(obj){
+				case TBSITES:
+					readAll(obj);
+					break;
+			}
+			console.log('数据删除成功');
+		};
+	}
+}
+
+//显示设置的所有数据
+function readAll(obj,src) {
+	if(db){
+		var arr=new Array();
+		var tran=db.transaction([obj],'readonly');
+		var objectStore =tran.objectStore(obj);
+		objectStore.openCursor().onsuccess = function (event) {
+			var cursor = event.target.result;
+			if (cursor) {
+				arr.push(cursor.value);
+				cursor.continue();
+			} else {
+				console.log('没有更多数据了！');
+			}
+		};
+		tran.oncomplete=function(event){
+			switch(obj){
+				case TBSITES:
+					var data=HandleData(arr);
+					ShowData(data);
+					break;
+				case TBTYPE:
+					ShowTypeStatus(arr);
+			}
+			
+		}
+	}else{
+		CreateDatabase(src);
+	}
+};
+
+//数据按照type分类
+function HandleData(arr){
+	console.log(arr);
+	var nlist = [];
+	for (var i = 0; i < arr.length; i++) {
+		console.log(arr[i].str);
+		if (nlist.length == 0) {
+			nlist.push({
+				type: arr[i].type,
+				data: [arr[i]]
+			})
+		} else {
+			var index = nlist.findIndex(item => item.type == arr[i].type);
+			if (index >= 0) {
+				nlist[index].data.push(arr[i])
+			} else {
+				nlist.push({
+					type: arr[i].type,
+					data: [arr[i]]
+				})
+			}
+		}
+	}
+	return nlist;
+}
+
+//显示自定义的网站数据
+function ShowData(data){
+	$("#selfcontent").children().remove();
+	for(var i=0;i<data.length;i++){
+		console.log(data);
+		var innercontent='<div class="tags" onclick="ChangeStatus"><span class="titlesign"></span><h3>'+data[i].type+'</h3></div><div class="sitediv"><ul id="'+i+'" class="sitesul"></ul></div>';
+		$("#selfcontent").append(innercontent);
+		
+		var resourcename='#'+i;
+		var fatherdom=$(resourcename);
+		for(var j=0;j<data[i].data.length;j++){
+			var x=Math.floor(Math.random()*100+1);
+			var y=Math.floor(Math.random()*100+1);
+			var z=Math.floor(Math.random()*100+1);
+
+			var firstname=data[i].data[j].name.substring(0,1);
+			var childdom='<li class="sitesli">';
+			childdom+='<i class="siteicon" style="background-color:#'+x+y+z+'">'+firstname+'</i>';
+			childdom+='<a class="sitetext" href="'+data[i].data[j].url+'">'+data[i].data[j].name+'</a>';
+			childdom+='<span  data="'+data[i].data[j].id+'" onclick="spanclick(this);"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-shanchu"></use></svg></span>';
+			childdom+='</li>';
+			fatherdom.append(childdom);
+		}
+		fatherdom.append("<li style='clear:both;'></li>");
+	}
+}
+
+//显示分类的显示状态
+function ShowTypeStatus(arr){
+	for(var i=0;i<arr.length;i++){
+		// console.log(arr[i]);
+		if(arr[i].canuse==false){
+			var dom="input[type='checkbox'][value='"+arr[i].defaulttype+"']";
+			// console.log(dom);
+			$(".ulsettypes").find(dom).prop('checked',false);
+		}
+	}
+}
+
+//显示主页的数据
+function ReadHomeData(obj){
+	if(db){
+		var arr=new Array();
+		var tran=db.transaction([obj],'readonly');
+		var objectStore =tran.objectStore(obj);
+		objectStore.openCursor().onsuccess = function (event) {
+			var cursor = event.target.result;
+			if (cursor) {
+				arr.push(cursor.value);
+				cursor.continue();
+			} else {
+				console.log('没有更多数据了！');
+			}
+		};
+		tran.oncomplete=function(event){
+			switch(obj){
+				case TBSITES:
+					
+					break;
+				case TBTYPE:
+					LoadUseData(arr);
+			}
+			
+		}
+	}else{
+		CreateDatabase("home");
+	}
+}
+
+//根据配置显示数据
+function LoadUseData(arr){
+	for(var i=0;i<arr.length;i++){
+		if(arr[i].canuse==false){
+			var classname='.'+arr[i].defaulttype;
+			var id='#'+arr[i].defaulttype;
+			console.log(classname);
+			$(classname).css("display","none");
+			$(id).css("display","none");
+		}else{
+			if(arr[i].defaulttype=="custom"){
+				readAll(TBSITES,"set");
+			}
+		}
+	}
 }
